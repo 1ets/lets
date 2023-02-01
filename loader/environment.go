1b7489562d0058df3479d25b0fe637de538e972a
Dhuta P <dhuta_venturo@loyalto.id>
2023-02-01 11:41:28 +0700

    FIX absolute path env file

--- loader/environment.go
+++ loader/environment.go
@@ -6,8 +6,18 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var EnvFile string
+
 // Loading .env environment variable into memory.
 func Environment() {
+	if EnvFile != "" {
+		err := godotenv.Load(EnvFile)
+		if err != nil {
+			log.Fatalln("Error loading .env file")
+		}
+		return
+	}
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalln("Error loading .env file")
